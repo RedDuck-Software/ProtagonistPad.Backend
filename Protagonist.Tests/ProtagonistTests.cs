989a886c3a6ac618c0316bec540cac8a82ab14c2
polishchukredduck <tymurpolishchukwork@gmail.com>
2022-06-03 23:10:11 +0300

    Set up a working environment for creating tests

---
+++ Protagonist.Tests/ProtagonistTests.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace ProtagonistTests;
+
+public class ProtagonistTests
+{
+    [Fact]
+    public void Test()
+    {
+        //Arrange
+        
+        //Actual
+
+        //Assert
+    }
+}
